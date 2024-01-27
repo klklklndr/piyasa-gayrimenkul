ee77efa8cd5bafeaf08db2310f50e6434309a68f
klklklndr <klndr.ercan@gmail.com>
2024-01-27 03:14:52 +0300

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import Head from 'next/head';
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
+const GTM_ID = 'GTM-PTCQ9VNK';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+          <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+    </Script>
     <Head>
-      <GoogleTagManager gtmId="GTM-PTCQ9VNK" />
 			<link rel='icon' href='/public/image/subicon.png' />
 		</Head>
-      <body className={inter.className}>{children}</body>
+      <body 
+      className={inter.className}>{children}
+      <noscript
+        dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+        }} 
+    />
+      </body>
     </html>
   );
 }
