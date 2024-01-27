e2e0f06e5fe4805466ce1aecd219ec6c464f0ea9
klklklndr <klndr.ercan@gmail.com>
2024-01-27 16:57:20 +0300

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Piyasa Gayrimenkul",
-  description: "Piyasa Gayrimenkul",
+  description: "İsa Kalender ve Kudret Çöçü'nün sunduğu Piyasa Gayrimenkul programı her pazar saat 20.00'de Lider Haber Kanalı'nda izleyicileriyle buluşuyor. Gayrimenkul piyasasındaki son gelişmeler, trendler ve ipuçlarına dair en güncel bilgileri bu programda bulabilirsiniz. İnşaat projelerinden emlak yatırımlarına, konut kredilerinden gayrimenkul vergilerine kadar geniş bir yelpazede gayrimenkul sektörüne dair her şeyi keşfedin. Kaçırmayın, Piyasa Gayrimenkul her pazar günü sizleri bekliyor",
 };
 
 export default function RootLayout({
