1e90281c90aa374ff1b5bae20145efcaa922dff9
klklklndr <klndr.ercan@gmail.com>
2024-01-27 03:47:21 +0300

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script'
 const GTM_ID = 'GTM-PTCQ9VNK';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,15 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
-          <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-    </Script>
+    <GoogleTagManager gtmId="GTM-PTCQ9VNK" />
     <Head>
 			<link rel='icon' href='/public/image/subicon.png' />
 		</Head>
